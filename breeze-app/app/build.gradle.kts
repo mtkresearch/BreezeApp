246bb061f52f2076c9520c508afcebbe12f2b9cc
Muxi Chen <41140021+muxi1998@users.noreply.github.com>
2025-06-05 06:56:20 +0000

    update warning content in intro. dialog

--- breeze-app/app/build.gradle.kts
+++ breeze-app/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.mtkresearch.breezeapp"
         minSdk = 33
         targetSdk = 35
-        versionCode = 26
-        versionName = "1.0.12"
+        versionCode = 27
+        versionName = "1.0.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
