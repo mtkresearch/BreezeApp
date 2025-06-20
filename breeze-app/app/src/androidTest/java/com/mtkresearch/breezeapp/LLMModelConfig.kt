09169c72f5bf4e94d59a02857fc7f41ebf129652
BrandonTeng <BrandonTeng@taiwanmobile.com>
2025-06-20 09:53:36 +0800

    Update LLMModelConfig. Use SerializedName annotation to improve the class attribute naming.

--- breeze-app/app/src/androidTest/java/com/mtkresearch/breezeapp/LLMModelConfig.kt
+++ breeze-app/app/src/androidTest/java/com/mtkresearch/breezeapp/LLMModelConfig.kt
@@ -1,14 +1,17 @@
 package com.mtkresearch.breezeapp
 
+import com.google.gson.annotations.SerializedName
+
 data class LLMModelConfig(
+    @SerializedName("models")
     val models: List<Model>
 ) {
     data class Model(
-        val id: String,
-        val runner: String,
-        val backend: String,
-        val ramGB: Int,
-        val model_entry_path: String,
-        val urls: List<String>
+        @SerializedName("id") val id: String,
+        @SerializedName("runner") val runner: String,
+        @SerializedName("backend") val backend: String,
+        @SerializedName("ramGB") val ramGB: Int,
+        @SerializedName("model_entry_path") val modelEntryPath: String,
+        @SerializedName("urls") val urls: List<String>
     )
 }
