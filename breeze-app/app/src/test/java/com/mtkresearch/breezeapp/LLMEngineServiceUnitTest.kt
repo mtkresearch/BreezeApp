8820ef31a895caf253910159cbc627d310062309
BrandonTeng <BrandonTeng@taiwanmobile.com>
2025-07-29 16:29:09 +0800

    Update Robolectric test for LLMEngineService. Replace doAnswer by doReturn for solving Mocking inside of mocking issue.

--- breeze-app/app/src/test/java/com/mtkresearch/breezeapp/LLMEngineServiceUnitTest.kt
+++ breeze-app/app/src/test/java/com/mtkresearch/breezeapp/LLMEngineServiceUnitTest.kt
@@ -7,8 +7,8 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.ArgumentMatchers.any
 import org.mockito.ArgumentMatchers.eq
-import org.mockito.Mockito.doAnswer
 import org.mockito.Mockito.doNothing
+import org.mockito.Mockito.doReturn
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.spy
 import org.mockito.Mockito.verify
@@ -23,34 +23,37 @@ class LLMEngineServiceUnitTest {
     @Test
     fun testLLMEngineServiceCodeCoverage() {
         val prompt = "Tell me a joke."
-        val llmParams = mock(LLMInferenceParams::class.java)
-        `when`(llmParams.maxToken).thenReturn(128)
-        `when`(llmParams.temperature).thenReturn(0.7f)
 
         // init LLMEngineService by Robolectric
         val controller = Robolectric.buildService(LLMEngineService::class.java)
         val service = controller.create().get()
         val spyService = spy(service)
+
+        // mock LLMInferenceParams
+        val llmParams = mock(LLMInferenceParams::class.java)
+        `when`(llmParams.maxToken).thenReturn(128)
+        `when`(llmParams.temperature).thenReturn(0.7f)
+
+        // mock LLMEngineService.isReady
         `when`(spyService.isReady).thenReturn(true)
 
         // mock future for LLMEngineService.initialize()
         `when`(spyService.initialize())
             .thenReturn(CompletableFuture.completedFuture(true))
 
-        // Stub StreamingResponseCallback
+        // mock StreamingResponseCallback
         val dummyCallback: (String) -> Unit =
             { token -> Log.d("tag", "dummy callback: $token") }
 
         // mock future for LLMEngineService.generateStreamingResponse()
-        doAnswer { invocation ->
-            CompletableFuture.completedFuture("final output")
-        }.`when`(spyService).generateStreamingResponse(
-            eq(prompt),
-            eq(llmParams),
-            any<(String) -> Unit>()
-        )
+        doReturn(CompletableFuture.completedFuture("final output"))
+            .`when`(spyService).generateStreamingResponse(
+                eq(prompt),
+                eq(llmParams),
+                any() // Still crucial for the callback
+            )
 
-        // Stub stopGeneration
+        // mock LLMEngineService.stopGeneration()
         doNothing().`when`(spyService).stopGeneration()
 
         // LLMEngineService Test Flow
