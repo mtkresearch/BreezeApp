35ae1b4e49551ca629c59c507ec64a7891e31cc0
BrandonTeng <BrandonTeng@taiwanmobile.com>
2025-08-01 16:01:34 +0800

    Update Robolectric test for TTSEngineService.

--- breeze-app/app/src/test/java/com/mtkresearch/breezeapp/TTSEngineServiceUnitTest.kt
+++ breeze-app/app/src/test/java/com/mtkresearch/breezeapp/TTSEngineServiceUnitTest.kt
@@ -7,10 +7,15 @@ import com.mtkresearch.breezeapp.service.TTSEngineService
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers.eq
+import org.mockito.Mockito.doNothing
+import org.mockito.Mockito.doReturn
 import org.mockito.Mockito.spy
 import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import java.util.concurrent.CompletableFuture
 
 @RunWith(RobolectricTestRunner::class)
 class TTSEngineServiceUnitTest {
@@ -18,6 +23,7 @@ class TTSEngineServiceUnitTest {
     @Test
     fun testLLMEngineServiceCodeCoverage() {
         val tag = "testLLMEngineServiceCodeCoverage"
+        val speakMessage = "今天天氣真好"
 
         // init TTSEngineService by Robolectric
         val controller = Robolectric.buildService(TTSEngineService::class.java)
@@ -42,6 +48,29 @@ class TTSEngineServiceUnitTest {
         Log.d(tag, "isReady:$isReady")
         verify(spyService).isReady
 
+        // mock future for TTSEngineService.initialize()
+        `when`(spyService.initialize())
+            .thenReturn(CompletableFuture.completedFuture(true))
+
+        // mock future for TTSEngineService.speak()
+        doReturn(CompletableFuture.completedFuture(true))
+            .`when`(spyService).speak(eq(speakMessage))
+
+        // mock future for TTSEngineService.initialize()
+        doNothing().`when`(spyService).stopSpeaking()
+
+        // TTSEngineService Test Flow
+        spyService.initialize().thenAccept { initResult ->
+            spyService.speak(speakMessage).thenAccept {
+                spyService.stopSpeaking()
+                verify(spyService).initialize()
+                verify(spyService).speak(speakMessage)
+                verify(spyService).stopSpeaking()
+            }
+        }
+
+
+        controller.destroy()
     }
 
 }
